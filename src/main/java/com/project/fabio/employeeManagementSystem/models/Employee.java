f4c057e67dc188c00562e7dcfb14965e24b67b4a
Fábio <fabio.goncalves@school.mindera.com>
2024-01-25 20:11:44 +0000

    feat(model): getters and setters added

--- src/main/java/com/project/fabio/employeeManagementSystem/models/Employee.java
+++ src/main/java/com/project/fabio/employeeManagementSystem/models/Employee.java
@@ -1,5 +1,10 @@
 package com.project.fabio.employeeManagementSystem.models;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class Employee {
     private Long id;
     private String firstName;
