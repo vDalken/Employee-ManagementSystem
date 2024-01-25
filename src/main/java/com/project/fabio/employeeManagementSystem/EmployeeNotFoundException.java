52723a838b7dcd8d14bca68e9e2f5bbf6752348b
Fábio <fabio.goncalves@school.mindera.com>
2024-01-25 20:06:21 +0000

    feat(service): service class structured

---
+++ src/main/java/com/project/fabio/employeeManagementSystem/EmployeeNotFoundException.java
@@ -0,0 +1,2 @@
+package com.project.fabio.employeeManagementSystem;public class EmployeeNotFoundException {
+}
--- src/main/java/com/project/fabio/employeeManagementSystem/services/EmployeeService.java
+++ src/main/java/com/project/fabio/employeeManagementSystem/services/EmployeeService.java
@@ -1,8 +1,47 @@
 package com.project.fabio.employeeManagementSystem.services;
 
+import com.project.fabio.employeeManagementSystem.EmployeeNotFoundException;
+import com.project.fabio.employeeManagementSystem.interfaces.EmployeeRepository;
+import com.project.fabio.employeeManagementSystem.models.Employee;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Objects;
 
 @Service
 public class EmployeeService {
+    @Autowired
+    private EmployeeRepository employeeRepository;
 
+    public List<Employee> getEmployeesList() {
+        return employeeRepository.findAll();
+    }
+
+    public Employee getEmployeeById(Long id) {
+        return employeeRepository.findById(id).orElseThrow(() -> new EmployeeNotFoundException());
+    }
+
+    public Employee createEmployee(Employee employee) {
+        return employeeRepository.save(employee);
+    }
+
+    public Employee updateEmployee(Long id, Employee employee) {
+        Employee employeeOptional = employeeRepository.findById(id).orElseThrow(() -> new EmployeeNotFoundException());
+        List<Employee> employeeList = this.getEmployeesList();
+        employeeList
+                .stream()
+                .filter(listEmployee -> Objects.equals(listEmployee.getId(), id))
+                .map(listEmployee -> {
+                    listEmployee.setFirstName(employee.getFirstName());
+                    listEmployee.setLastName(employee.getLastName());
+                    listEmployee.setEmail(employee.getEmail());
+                    return listEmployee;
+                });
+        return employee;
+    }
+
+    public void deleteEmployee(Long id) {
+        employeeRepository.deleteById(id);
+    }
 }
